670242d551959c9752397ece7c8fa7604a030d80
muflihun <muflihun@firmanedam19@gmail.com>
2020-06-20 02:14:20 +0700

    Model Auth

---
+++ application/models/M_user.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_user extends CI_Model {
+    public function register()
+    {
+        $data = array(
+            'nama' => $this->input->post('nama',true),
+            'username' => $this->input->post('username','true'),
+            'password' => password_hash($this->input->post('password',true), PASSWORD_DEFAULT),
+            'alamat' => $this->input->post('alamat',true),
+            'notelp' => $this->input->post('notelp',true),
+            'role' => 'karyawan'
+        );
+    }
+}
+?>
