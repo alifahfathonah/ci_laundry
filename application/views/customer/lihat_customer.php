c30f2d620d2ec6d0e2dc48869a500c69879ed7df
muflihun <muflihun@firmanedam19@gmail.com>
2020-06-23 19:19:40 +0700

    Customer Dan Karyawan

---
+++ application/views/customer/lihat_customer.php
@@ -0,0 +1,54 @@
+<div id="wrapper">
+	<div id="content-wrapper" class="d-flex flex-column">
+
+		<!-- Main Content -->
+		<div id="content">
+			<div class="container-fluid">
+			<?= $this->session->flashdata('message'); ?>
+				<!-- Page Heading -->
+				<h1 class="h3 mb-2 text-gray-800">Lihat Customer</h1>
+				<div class="card shadow mb-2">
+					<div class="card-header py-3">
+						<!-- <h6 class="m-0 font-weight-bold text-primary">Kategori</h6> -->
+						<a class="btn btn-primary float-right" href="<?= base_url('home/tambah_customer') ?>">Tambah Customer</a>
+					</div>
+					<div class="card-body">
+						<div class="table-responsive">
+							<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+								<thead>
+									<tr>
+										<th>No</th>
+										<th>Nama</th>
+										<th>Alamat</th>
+										<th>Nomor Hp</th>
+										<th>Opsi</th>
+
+									</tr>
+								</thead>
+								<?php $no = 0; ?>
+								<tbody>
+									<?php foreach ($customer as $item) { ?>
+									<tr>
+										<td><?= ++$no ?></td>
+										<td><?= $item['nama']; ?> </td>
+										<td><?= $item['alamat']; ?> </td>
+										<td><?= $item['no_hp']; ?> </td>
+										<td><a href="<?= base_url('home/editCustomer/'.$item['id_customer']); ?>" class="btn btn-primary"><i class="fa fa-sm fa-edit"></i>Edit</a>
+                                        <a href="<?= base_url('home/hapusCustomer/'.$item['id_customer']) ?>" class="btn btn-danger" onclick="return confirm('Anda Yakin?'); "><i class="fa fa-sm fa-trash"></i>Hapus</a>
+                                        </td>
+									</tr>
+									<?php } ?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+</div>
+<a class="scroll-to-top rounded" href="#page-top">
+	<i class="fas fa-angle-up"></i>
+</a>
---
+++ application/views/customer/tambah-customer.php
@@ -0,0 +1,29 @@
+<div class="container">
+	<div class="row">
+		<div class="card col-md-6 offset-md-3">
+			<div class="card-header">
+				<h2>Tambah Customer</h2>
+			</div>
+			<div class="card-body">
+				<form action="<?= base_url('home/tambah_customer') ?>" method="POST">
+					<div class="form-group">
+						<label >Nama:</label>
+						<input type="text" class="form-control" id="uname" placeholder="Nama Customer" name="nama" required>
+						<small><?= form_error('nama') ?> </small>
+					</div>
+					<div class="form-group">
+						<label >Alamat Lengkap:</label>
+						<input type="text" class="form-control" placeholder="Alamat Lengkap" name="alamat" required>
+						<small><?= form_error('alamat') ?> </small>
+					</div>
+					<div class="form-group">
+						<label >No Hp:</label>
+						<input type="text" class="form-control" id="uname" placeholder="No Hp" name="nohp" required>
+						<small><?= form_error('nohp') ?> </small>
+					</div>
+					<button type="submit" class="btn btn-primary">Simpan</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
